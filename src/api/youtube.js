0ba8483666ea6bacbe351018d5eb2df95f51bcaa
Stanley Luong <slluong@uw.edu>
2025-05-17 22:32:16 -0700

    amplify deploy

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL || "https://www.googleapis.com/youtube/v3",
   params: {
     part: "snippet",
     maxResults: 20,
--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as SearchBar } from "./SearchBar";
-export { default as VideoDetail } from "./VideoDetail";
-export { default as VideoItem } from "./VideoItem";
-export { default as VideoList } from "./VideoList";
